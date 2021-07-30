3bcdea2a34dd78060af116c4c1696f768870fdc6
Dakota O'Neil <oneilkody@gmail.com>
2021-07-30 08:41:23 -0500

    add bootstrap and layout the rest of the game functionality in comments

--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,9 @@ const p1Span = document.getElementById('playerOneCard')
 const p2Span = document.getElementById('playerTwoCard')
 const resultSpan = document.getElementById('result')
 
+let pile1 = [].flat()
+        let pile2 = [].flat()
+
 function cardVal(card) {
     if (card === "ACE") return 14
     else if (card === 'KING') return 13
@@ -20,6 +23,9 @@ function suitVal(suit) {
     else if (suit === "HEARTS") return 3
     else if (suit === "HEARTS") return 2
     else return 1
+}
+function addToPile(cards, pile) {
+    cards.forEach(el=>pile.push(el))
 }
 
 async function newDeck() {
@@ -75,18 +81,38 @@ async function draw() {
         if (cardVal(card[0].value) > cardVal(card[1].value)) {
             console.log('Player 1 wins!')
             resultSpan.innerText = 'Player 1 wins!'
+            addToPile([card[0],card[1]], pile1)
+            document.getElementById('playerOnePile').innerText = pile1.length
         } else if (cardVal(card[1].value) > cardVal(card[0].value)) {
             console.log('Player 2 wins!')
             resultSpan.innerText = 'Player 2 wins!'
-        } else if (suitVal(card[0].suit) > suitVal(cardd[1].suit)) {
+            addToPile([card[0],card[1]], pile2)
+            document.getElementById('playerTwoPile').innerText = pile2.length
+        } else if (suitVal(card[0].suit) > suitVal(card[1].suit)) {
             console.log('Player 1 wins by suit!')
             resultSpan.innerText = 'Player 1 wins by suit!'
+            addToPile([card[0],card[1]], pile1)
+            document.getElementById('playerOnePile').innerText = pile1.length
         } else {
             console.log('Player 2 wins by suit!')
             resultSpan.innerText = 'Player 2 wins by suit!'
+            addToPile([card[0],card[1]], pile2)
+            document.getElementById('playerTwoPile').innerText = pile2.length
         }
+        document.getElementById('cardsLeft').innerText = 'There are ' + data.remaining + ' cards left.'
+
+
     }
     catch(err){
         console.log(err)
     }
+
+    // if (data.remaining)
 }
+
+/* 
+- need to seperate out the draw function so it is not all just inside there 
+- function called 'deal' that takes the initial deck and deals it to each players' hand
+- function that when someone's hand reaches 0 from playing, they shuffle their pile and that becomes their hand, then they can continue playing
+- conditional that when one player gets 52 cards they win!
+*/
