6ccd6faf0b2a0fdccbcd046ac863c522886ece95
Dakota O'Neil <oneilkody@gmail.com>
2021-08-02 22:43:39 -0500

    finish functionality for in-browser front end game that is very basic

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 document.querySelector('#newDeck').addEventListener('click', newDeck)
 document.querySelector('#shuffleDeck').addEventListener('click', shuffleDeck)
 document.querySelector('#draw').addEventListener('click', draw)
+// document.querySelector('#deal').addEventListener('click', deal)
 
 const url = 'https://deckofcardsapi.com/api/deck/'
 let deckID 
@@ -8,8 +9,51 @@ const p1Span = document.getElementById('playerOneCard')
 const p2Span = document.getElementById('playerTwoCard')
 const resultSpan = document.getElementById('result')
 
+let hand1 = 'hand1'
+let hand2 = 'hand2'
+
 let pile1 = [].flat()
-        let pile2 = [].flat()
+let pile2 = [].flat()
+
+async function newDeck() {
+    const newOne = url + 'new/shuffle/?deck_count=1'
+
+    try {
+        const response = await fetch(newOne)
+        const data = await response.json()
+        deckID = data.deck_id
+        console.log(data)
+        console.log([data.deck_id])
+        
+        document.querySelector('.loot').classList.remove('hidden')
+        
+        if (pile1.length === 0) {
+            document.querySelector('#playerOneLoot').innerText = 0
+        }
+        if (pile2.length === 0) {
+            document.querySelector('#playerTwoLoot').innerText = 0
+        }
+
+        // for (let i = 1 ; i <= 52 ; i++) {
+        //     let pileNum 
+        //     if (i % 2 != 0) {
+        //         pileNum = 1
+        //     } else {
+        //         pileNum = 2
+        //     }
+            
+        //     const dealURL = url + deckID + '/pile/' + 'hand' + pileNum + '/add/?count=1'
+        //     const res = await fetch(dealURL)
+        //     console.log(pile1)
+        // }
+
+        // document.querySelector('#p1Hand').innerText = pile1.length
+        // document.querySelector('#p2Hand').innerText = pile2.length
+    }
+    catch(err){
+      console.log(`error ${err}`)
+    }
+}
 
 function cardVal(card) {
     if (card === "ACE") return 14
@@ -28,21 +72,6 @@ function addToPile(cards, pile) {
     cards.forEach(el=>pile.push(el))
 }
 
-async function newDeck() {
-    const newOne = url + 'new/shuffle/?deck_count=1'
-    
-    try {
-        const response = await fetch(newOne)
-        const data = await response.json()
-        deckID = data.deck_id
-        console.log(data)
-        console.log([data.deck_id])
-    }
-    catch(err){
-      console.log(`error ${err}`)
-    }
-}
-
 async function shuffleDeck() {
     const shuffle = url + deckID + '/shuffle/'
     
@@ -57,16 +86,27 @@ async function shuffleDeck() {
     }
 }
 
+// async function deal() {
+//     const dealURL = url + deckID + '/pile/' + pile + '/add/?count=1'
+//     try {
+//         const res = await fetch(dealURL)
+//         const data = await res.json()
+        
+//     } catch(err) {
+//         console.log(err)
+//     }
+// }
+
 async function draw() {
     const drawURL = url + deckID + '/draw/?count=2'
     try {
         const response = await fetch(drawURL)
         const data = await response.json()
         console.log(data)
-        // console.log(data.cards.forEach(el=> {
-        //     let i = data.cards.indexOf(el)
-        //     console.log('Card ' + i + ' is the ' + el.value + ' of ' + el.suit)
-        // }))
+    
+        document.querySelector('.showDraw1').classList.remove('hidden')
+        document.querySelector('.showDraw2').classList.remove('hidden')
+
         const card = data.cards
         const p1Card = card[0].value + ' of ' + card[0].suit
         const p1CardImg = card[0].image
@@ -79,35 +119,39 @@ async function draw() {
         p2CardImage.src = p2CardImg
 
         if (cardVal(card[0].value) > cardVal(card[1].value)) {
-            console.log('Player 1 wins!')
-            resultSpan.innerText = 'Player 1 wins!'
+            resultSpan.innerText = 'Player 1 wins the round!'
             addToPile([card[0],card[1]], pile1)
-            document.getElementById('playerOnePile').innerText = pile1.length
+            document.getElementById('playerOneLoot').innerText = pile1.length
         } else if (cardVal(card[1].value) > cardVal(card[0].value)) {
-            console.log('Player 2 wins!')
-            resultSpan.innerText = 'Player 2 wins!'
+            resultSpan.innerText = 'Player 2 wins the round!'
             addToPile([card[0],card[1]], pile2)
-            document.getElementById('playerTwoPile').innerText = pile2.length
-        } else if (suitVal(card[0].suit) > suitVal(card[1].suit)) {
-            console.log('Player 1 wins by suit!')
-            resultSpan.innerText = 'Player 1 wins by suit!'
+            document.getElementById('playerTwoLoot').innerText = pile2.length
+        } else if (cardVal(card[1].value) === cardVal(card[0].value) && suitVal(card[0].suit) > suitVal(card[1].suit)) {
+            resultSpan.innerText = 'Player 1 wins the round by suit!'
             addToPile([card[0],card[1]], pile1)
-            document.getElementById('playerOnePile').innerText = pile1.length
-        } else {
-            console.log('Player 2 wins by suit!')
-            resultSpan.innerText = 'Player 2 wins by suit!'
+            document.getElementById('playerOneLoot').innerText = pile1.length
+        } else if (cardVal(card[1].value) === cardVal(card[0].value) && suitVal(card[1].suit) > suitVal(card[0].suit)){
+            resultSpan.innerText = 'Player 2 wins the round by suit!'
             addToPile([card[0],card[1]], pile2)
-            document.getElementById('playerTwoPile').innerText = pile2.length
+            document.getElementById('playerTwoLoot').innerText = pile2.length
+        } else {
+            console.log('Unexpected result.')
+            resultSpan.innerText = 'Unexpected result.'
         }
 
         document.getElementById('cardsLeft').innerText = 'There are ' + data.remaining + ' cards left.'
 
+        if (data.remaining === 0 && pile1.length > pile2.length) {
+            document.querySelector('#p1Win').classList.toggle('hidden')
+        } else if (data.remaining === 0 && pile2.length > pile1.length){
+            document.querySelector('#p2Win').classList.toggle('hidden')
+        }
     }
     catch(err){
         console.log(err)
     }
-    // if (data.remaining)
 }
+
 
 
 /* 
